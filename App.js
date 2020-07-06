b4e3c32ace0eecd2cc8d069ef61068d65699f8ee
sanjuron <julien_nn@live.fr>
2020-07-06 16:16:16 +0200

    rendu liste / ajout icone

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component} from 'react';
 import { Button, FlatList, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
-export default function App() {
+export default class App extends Component{
+state = {
+  todos : [
+   {id:1, post:"Regarder un film"},
+   {id:2, post:"Réviser Native"},
+  ],
+  task : "",
+}
+
+addTodo = (todo) => {
+  let todos= [...this.state.todos, todo];
+  this.setState({
+    todos : todos
+  })
+  console.log(this.state.todos)
+}
+
+addTask = (task) => {
+  this.setState({
+    task : task
+  })
+  console.log(this.state.task)
+}
+
+deleteTodo = (id) => {
+  let todos = this.state.todos.filter(todo => {
+    return todo.id !== id
+  }); //filter est une méthode non destructrice, elle n'altère pas le state
+  this.setState({
+    todos: todos 
+  })
+}
+
+
+render () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Add a new Todo!</Text>
+      <MaterialCommunityIcons name="delete" size={24} color="black" />
+      <TextInput style={styles.input} value={this.state.task} onChangeText={text => this.addTask(text)} />
+      <Button title="valider" onPress={this.addTodo} />
+
+      <FlatList
+        data={this.state.todos}
+        renderItem={({item}) => <Text style={styles.list}>{item.post}</Text> }        
+        keyExtractor={item => item.id.toString()}
+      />
+    
     </View>
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#33D1FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 10,
+    margin: 5,
+  },
+  list:{
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 15,
+    margin: 5,
+    width: 300,
+  }
 });
