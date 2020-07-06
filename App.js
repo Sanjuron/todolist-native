c6708b796ae80354d510f1d4c7529a237735f497
sanjuron <julien_nn@live.fr>
2020-07-06 14:16:45 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
